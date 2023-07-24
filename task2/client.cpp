01fbf009b3b136ee228278b5af9f6f9f22c225e4
wyp <1257878710@qq.com>
2023-07-24 17:34:17 +0800

    homework

--- task2/client.cpp
+++ task2/client.cpp
@@ -3,7 +3,7 @@
 #include<iostream>
 #pragma comment(lib, "ws2_32.lib") 
 using namespace std;
-SOCKET serverSocket;
+SOCKET serverSocket,loginSocket;
 
 void mysend(){
     char buff[56];
@@ -16,48 +16,76 @@ void mysend(){
 void myreceive(){
     char temp[60];
     while(1){
-    // cout<<"*"<<endl;
     int r=recv(serverSocket,temp,59,0);
-    // cout<<"r="<<r<<endl;
     if(r>0)
     {
         temp[r]=0;
         cout<<temp<<endl;
     }
-    // cout<<"#"<<endl;
     }
 }
 
+bool login(){
+    string username,password;
+    cout<<"请输入用户名："<<endl;
+    cin>>username;
+    cout<<"请输入密码："<<endl;
+    cin>>password;
+    string temp=(username+' '+password);
+    send(loginSocket,temp.c_str(),strlen(temp.c_str()),0);
+    char ans[5];
+    int r=recv(loginSocket,ans,5,0);
+    if(r>0)
+    {        
+        if(ans[0]=='1')
+        {
+            cout<<"登录成功"<<endl;
+            return true;
+        }
+        else
+        {
+            cout<<"登录失败"<<endl;
+            return false;
+        }
+        }
+    return false;
+}
 
-int main()
-{
+void createsocket(SOCKET &sock,int port){
     //1.确定网络协议版本
     WSADATA wsaData;
     WSAStartup(MAKEWORD(2, 2), &wsaData);
 
     //2.创建socket
-    serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
     //3.确定服务器协议地址簇
     SOCKADDR_IN addr = { 0 };
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    addr.sin_port = htons(9527);
+    addr.sin_port = htons(port);
 
     //4.连接服务器
-    int r = connect(serverSocket, (sockaddr*)&addr, sizeof(addr));
+    int r = connect(sock, (sockaddr*)&addr, sizeof(addr));
     if (r == -1) {
         cout << "连接服务器失败" << endl;
-        closesocket(serverSocket);
+        closesocket(sock);
         WSACleanup();
         system("pause");
-        return -1;
     }
-    cout << "连接服务器成功" << endl;
+}
 
+int main()
+{
+    createsocket(loginSocket,9528);
+    cout << "连接服务器成功" << endl;
+    while(!login());
+    closesocket(loginSocket);
     //5.通信
+    createsocket(serverSocket,9527);
     CreateThread(NULL,NULL,(LPTHREAD_START_ROUTINE)mysend,NULL,NULL,NULL);
-    myreceive();
+    CreateThread(NULL,NULL,(LPTHREAD_START_ROUTINE)myreceive,NULL,NULL,NULL);
+    while(1){}
 
     closesocket(serverSocket);
     WSACleanup();
--- task2/server.cpp
+++ task2/server.cpp
@@ -3,63 +3,97 @@
 #include<iostream>
 #pragma comment(lib, "ws2_32.lib") 
 using namespace std;
-#define NUM 10
-SOCKET clientSocket[NUM];
+#define NUM 100
+SOCKET clientSocket[NUM],serverSocket,loginSocket,checkSocket[NUM];
 int count=0;
 
 void trans(int id){
     char buff[56];
     char temp[80];
     while(1){
-            // cout<<"*"<<endl;
         int r=recv(clientSocket[id],buff,55,0);
         if(r>0){
             buff[r]=0;
-            // cout<<buff<<endl;
             memset(temp,0,80);
             sprintf(temp,"用户%d:%s",id,buff);
             for(int i=0;i<count;i++){
                 send(clientSocket[i],temp,strlen(temp),0);
-                // cout<<"send to "<<i<<endl;
             }
         }
-            // cout<<"temp:"<<temp<<endl;
-            // cout<<"#"<<endl;
+        else{
+            cout<<"用户"<<id<<"已退出聊天"<<endl;
+            break;
+        }
     }
 }
 
-int main()
-{
+char checkuser(char* s){
+    string str=s;
+    if(str=="abc 123")
+    return '1';
+    else
+    return '0';
+}
+
+void check(int i){
+     while(1){
+        char ans[5]={'1'};
+        char buff[50];
+        int r=recv(checkSocket[i],buff,50,0);
+        ans[0]=checkuser(buff);
+        if(r>0){
+            send(checkSocket[i],ans,5,0);
+        }
+}
+}
+
+void checkThread(){
+    for(int i=0;i<NUM;i++)
+    {
+        checkSocket[i]=accept(loginSocket,NULL,NULL);
+        CreateThread(NULL,NULL,(LPTHREAD_START_ROUTINE)check,(LPVOID)i,NULL,NULL);
+    }
+}
+
+void serveThread(){
+    for(int i=0;i<NUM;i++){
+        clientSocket[i]=accept(serverSocket,NULL,NULL);
+        CreateThread(NULL,NULL,(LPTHREAD_START_ROUTINE)trans,(LPVOID)i,NULL,NULL);
+        count++;
+    }
+}
+
+void createsocket(SOCKET &sock, int port){
     //1.确定网络协议版本
     WSADATA wsaData;
     WSAStartup(MAKEWORD(2,2),&wsaData);
     
     //2.创建socket
-    SOCKET serverSocket=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+    sock=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
 
     //3.确定服务器协议地址簇
     SOCKADDR_IN addr={0};
     addr.sin_family=AF_INET;
     addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    addr.sin_port=htons(9527);
+    addr.sin_port=htons(port);
 
     //4.绑定
-    int r=bind(serverSocket,(sockaddr*)&addr,sizeof(addr));
+    int r=bind(sock,(sockaddr*)&addr,sizeof(addr));
 
     //5.监听
-    r=listen(serverSocket,10);
+    r=listen(sock,10);
     cout<<"服务器启动"<<endl;
+}
 
-    //6.接受
-    for(int i=0;i<NUM;i++){
-        clientSocket[i]=accept(serverSocket,NULL,NULL);
-        //7.通信
-        CreateThread(NULL,NULL,(LPTHREAD_START_ROUTINE)trans,(LPVOID)i,NULL,NULL);
-        cout<<"用户"<<i<<"加入"<<endl;
-        count++;
-    }
+int main()
+{
+    createsocket(loginSocket,9528);
+    createsocket(serverSocket,9527);
 
+    CreateThread(NULL,NULL,(LPTHREAD_START_ROUTINE)checkThread,NULL,NULL,NULL);
+    CreateThread(NULL,NULL,(LPTHREAD_START_ROUTINE)serveThread,NULL,NULL,NULL);
 
+    while(1);
 
     closesocket(serverSocket);
     WSACleanup();
